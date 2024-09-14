b6427728875e1341d409a0771518fc20b99d8496
avacode-de <gigaunt.jn@gmail.com>
2024-09-14 14:46:54 +0200

    added resume model

--- src/models.py
+++ src/models.py
@@ -1,6 +1,8 @@
-from sqlalchemy import Table, Column, Integer, String, MetaData
+import datetime
+from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey, text
 from sqlalchemy.orm import Mapped, mapped_column
 from database import Base
+import enum
 
 class WorkersOrm(Base):
     __tablename__ = "workers"
@@ -8,9 +10,19 @@ class WorkersOrm(Base):
     id: Mapped[int] = mapped_column(primary_key = True)
     username: Mapped[str]
 
+class WorkLoadOrm(enum.Enum):
+    parttime = "parttime"
+    fulltime = "fulltime"
 
+class ResumesOrm(Base):
+    __tablename__ = "resumes"
 
-
+    id: Mapped[int] = mapped_column(primary_key = True)
+    title: Mapped[str]
+    compensasion: Mapped[int] = mapped_column(nullable=True) #can equals Null also
+    workload: Mapped[WorkLoadOrm]
+    worker_id: Mapped[int] = mapped_column(ForeignKey("workers.id", ondelete="CASCADE")) #ForeignKey may bind few models
+    created_at: Mapped[datetime.datetime] = mapped_column(server_default=text("TIMEZONE('utc', now())"))
 
 
 
